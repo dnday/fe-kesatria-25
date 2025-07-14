a8aab4a80f61f7de1ee8638eeb7118fb290bd84a
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2025-07-14 16:43:49 +0700

    feat: add not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,39 @@
+"use client";
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 export default function Custom404() {
-  return(
+  useEffect(() => {
+    AOS.init({
+      duration: 800,    
+      once: true,      
+    })
+  }, [])
+
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-6xl font-bold text-gray-800">404</h1>
-      <p className="mt-4 text-xl text-gray-600">Page Not Found</p>
-      <a href="/" className="mt-6 text-blue-500 hover:underline">
-        Go back to Home
+      <h1
+        className="text-[8vw] font-bold text-[#A01326]"
+        data-aos="fade-down"         
+        data-aos-offset="200"        
+      >
+        404
+      </h1>
+      <p
+        className="mt-[-2.5vw] text-[1.5vw] font-bold text-[#013047]"
+        data-aos="fade-up"             
+        data-aos-delay="200"          
+      >
+        Page Not Found
+      </p>
+      <a
+        href="/"
+        className="mt-6 text-[1vw] text-blue-500 hover:underline"
+        data-aos="fade-up"             
+        data-aos-delay="400"          
+      >
+        Go back to home
       </a>
     </div>
   )
