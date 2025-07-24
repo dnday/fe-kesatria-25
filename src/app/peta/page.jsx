c84062afbab094a0bf0e5a1c46b8a1b4fc7183d7
dnday <marceldino9@gmail.com>
2025-07-24 12:24:40 +0700

    feat: add link to 3D Maps for Fakultas Teknik in Peta component and remove SpeedInsights from RootLayout

--- src/app/peta/page.jsx
+++ src/app/peta/page.jsx
@@ -76,6 +76,16 @@ export default function Peta() {
             <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-storybook text-[#013047] text-center lg:text-right mb-2 mt-5 sm:mb-3 md:mb-4">
               3D Maps Fakultas Teknik
             </h1>
+            <div className="flex justify-center lg:justify-end mt-4">
+              <a
+                href="https://maps.google.com/maps?q=Fakultas+Teknik+UGM&hl=id&ll=-7.770456,110.377930&spn=0.005,0.005&t=k&z=17"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-[#A01326] text-[#F9C157] font-storybook px-[4vw] py-[2vw] rounded-4xl text-[3vw] sm:text-[1.5vw] sm:py-[1vw] sm:px-[2vw] font-bold shadow-[inset_5px_5px_10px_rgba(0,0,0,0.3)] hover:bg-[#7f0818] cursor-pointer transition-colors duration-300"
+              >
+                3D Maps
+              </a>
+            </div>
           </div>
 
           {/* Map container - scaled proportionally for each breakpoint */}
